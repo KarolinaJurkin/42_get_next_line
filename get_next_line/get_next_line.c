18213df8d49ba3713826174f7066cce81e6c2483
Karolina Jurkin <kjurkin@c3r5s3.42warsaw.pl>
2025-03-07 19:25:01 +0100

    getting closer, but not too close

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kjurkin <kjurkin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 16:41:22 by kjurkin           #+#    #+#             */
-/*   Updated: 2025/03/06 18:40:55 by kjurkin          ###   ########.fr       */
+/*   Updated: 2025/03/07 19:23:08 by kjurkin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,80 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 20
+
+char	*ft_strchr(char *s, int c)
+{
+	int		i;
+
+	i = 0;
+	while (s[i] != c % 256 && s[i] != '\0')
+	{
+		i++;
+	}
+	if (s[i] == '\0' && c != '\0')
+		return (NULL);
+	return ((char *)(&s[i]));
+}
+
+int	count(char *buffer)
+{
+	int	i;
+
+	i = 0;
+	while (buffer[i] != '\n')
+		i++;
+	printf("i = %d\n", i);
+	return (i + 1);
+}
 
 char	*get_next_line(int fd)
 {
 	ssize_t	a;
 	char	*buffer;
+	char	*final;
+	
 	
 	buffer = malloc(BUFFER_SIZE);
 	a = read(fd, buffer, BUFFER_SIZE - 1);
-	while (a > 0)
+	final = ft_strchr(buffer, '\n'); // copy string to final, so after the change of buffer it still points to excess 
+	while (a > 0 && !ft_strchr(buffer, '\n'))
 	{
-		buffer[a] = '\0';
+		printf("a: %zd\n", a);
+		// buffer[a] = '\0';
 		printf("%s\n", buffer);
 		a = read(fd, buffer, BUFFER_SIZE - 1);
 	}
+	buffer[count(buffer) + 1] = '\0';
+	// final = malloc(count(buffer) + 1);
+	// final = buffer;
+	printf("%s\n", buffer);
 	return (buffer);
 }
+
+// char	*get_next_line(int fd)
+// {
+// 	ssize_t	a;
+// 	char	*buffer;
+// 	char	*final;
+	
+	
+// 	buffer = malloc(BUFFER_SIZE);
+// 	a = read(fd, buffer, BUFFER_SIZE - 1);
+// 	while (a > 0)
+// 	{
+// 		if (ft_strchr(buffer, '\n'))
+// 		{
+// 			read(fd, buffer, count(buffer));
+// 			printf("%s\n", buffer);
+// 		}
+// 		printf("a: %zd\n", a);
+// 		// buffer[a] = '\0';
+// 		printf("%s\n", buffer);
+// 		a = read(fd, buffer, BUFFER_SIZE - 1);
+// 	}
+// 	return (buffer);
+// }
 
 
 int	main(int argc, char *argv[])
